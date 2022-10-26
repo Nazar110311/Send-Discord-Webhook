8d5f42560ca68a254bc85d9406152ce00f339bd0
Nazar_Ilchenko <110988355+Nazar110311@users.noreply.github.com>
2022-10-26 23:22:10 +0300

    Send Message/Files in discord

---
+++ SendDiscordWebhook/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SendDiscordWebhook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SendWeebhook sw = new SendWeebhook();
+            sw.SendMessage("Your message", "New message!"); //1. message 2. title
+            sw.SendFile("sample3.txt", "file.txt"); //1. path to file 2. file name
+        }
+    }
+}
---
+++ SendDiscordWebhook/SendWeebhook.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+namespace SendDiscordWebhook
+{
+    class SendWeebhook
+    {
+        private readonly WebClient dWebClient = new WebClient();
+        private static NameValueCollection discord = new NameValueCollection();
+        public string WebHook = "https://discord.com/api/webhooks/1034920586822033479/2brRLAa752rKuzHMbMWOiqhVhjK3xCvEEt3wGlN9OzJL-B7CJVwkIORVh5lLYBAFZ3PE";
+
+        public void SendMessage(string msgSend, string title)
+        {
+            WebRequest wr = (HttpWebRequest)WebRequest.Create(WebHook);
+
+            wr.ContentType = "application/json";
+            wr.Method = "POST";
+
+            using (var sw = new StreamWriter(wr.GetRequestStream()))
+            {
+                string json = JsonConvert.SerializeObject(new
+                {
+                    embeds = new[]
+                    {
+                        new
+                        {
+                            title = title,
+                            description = msgSend
+                        }
+                    }
+                });
+                sw.Write(json);
+            }
+            var response = (HttpWebResponse)wr.GetResponse();
+        }
+        
+        public void SendFile(string filepath, string filename)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                MultipartFormDataContent form = new MultipartFormDataContent();
+                var file_bytes = System.IO.File.ReadAllBytes(filepath);
+                form.Add(new ByteArrayContent(file_bytes, 0, file_bytes.Length), filename, filename);
+                httpClient.PostAsync(WebHook, form).Wait();
+                httpClient.Dispose();
+            }
+        }
+    }
+}
